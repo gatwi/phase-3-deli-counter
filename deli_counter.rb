1efc6574e98ce2a61b485182e28adf740254c4be
gatwi <faith.murithi@student.moringaschool.com>
2022-09-02 14:38:38 +0300

    using arrays and enumerables together

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
 # Write your code here.
+
+# #line method- which should be empty
+def line(deli)
+    if deli.empty?
+      puts "The line is currently empty."
+    else
+      current_line = "The line is currently:"
+      deli.each.with_index(1) do |person, i|
+        current_line << " #{i}. #{person}"
+      end
+      puts current_line
+    end
+  end
+
+  # method to assign numbers to the incoming customers
+  def take_a_number(deli, name)
+    deli << name
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
+  end
+
+  # #now_serving method to call out them customers in line
+  def now_serving(deli)
+    if deli.empty?
+      puts "There is nobody waiting to be served!"
+    else
+      puts "Currently serving #{deli.first}."
+      deli.shift
+    end
+  end
